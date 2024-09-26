55fad1108a6d0d8ec92bfeb6c9f9ad1d2c59cc24
pronounceit-anya <aines@bu.edu>
2024-09-25 21:09:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
-
+import gcsa
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+from gcsa.event import Event
+from gcsa.google_calendar import GoogleCalendar
+from gcsa.recurrence import Recurrence, DAILY, SU, SA
+
+from beautiful_date import Jan, Apr
+
+calendar = GoogleCalendar('your_email@gmail.com')
+event = Event(
+    'Breakfast',
+    start=(1 / Jan / 2019)[9:00],
+    recurrence=[
+        Recurrence.rule(freq=DAILY),
+        Recurrence.exclude_rule(by_week_day=[SU, SA]),
+        Recurrence.exclude_times([
+            (19 / Apr / 2019)[9:00],
+            (22 / Apr / 2019)[9:00]
+        ])
+    ],
+    minutes_before_email_reminder=50
+)
+
+calendar.add_event(event)
+
+for event in calendar:
+    print(event)
